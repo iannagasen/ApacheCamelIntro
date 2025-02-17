5b9444a4287a38204af3fa99e8c570f9d1616727
ianneil.agasen <iann.agasen@gmail.com>
2025-02-17 22:12:35 +0800

    first commit

---
+++ src/main/java/dev/agasen/FileCopierWIthCamel.java
@@ -0,0 +1,26 @@
+package dev.agasen;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileCopierWIthCamel {
+
+    public static void main(String args[]) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new Route());
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+    }
+
+    static class Route extends RouteBuilder {
+
+        // in just 2 lines we create a file polling
+        @Override
+        public void configure() throws Exception {
+            from("file:data/inbox?noop=true")
+                .to("file:data/outbox");
+        }
+    }
+}
