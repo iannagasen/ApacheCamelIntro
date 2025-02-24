0221bd922581ea32b6e4d520130b8429690d1da3
ianneil.agasen <iann.agasen@gmail.com>
2025-02-24 22:46:05 +0800

    multicast and recipientList EIP

--- src/main/java/dev/agasen/chapter2/FtpJMs.java
+++ src/main/java/dev/agasen/chapter2/FtpJMs.java
@@ -1,13 +1,11 @@
 package dev.agasen.chapter2;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.RoutesBuilder;
+import org.apache.camel.*;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.language.XPath;
 
 import javax.jms.ConnectionFactory;
 
@@ -33,18 +31,129 @@ public class FtpJMs {
 //        context.addRoutes(new DefaultRoute());
 //        context.addRoutes(new CBRTestRoute());
 //        context.addRoutes(new RoutingAfterCBR());
-        context.addRoutes(new CBRTestRouteWithFilter());
+//        context.addRoutes(new CBRTestRouteWithFilter());
+        context.addRoutes(new CBRTestRouteWithFilterAndMulticast());
+
 
         context.start();
         Thread.sleep(100000);
         context.stop();
     }
 
-    static class CBRTestRouteWithFilter extends RouteBuilder {
+    // using RecipientList
+    static class RecipientListRoute extends RouteBuilder {
 
         @Override
         public void configure() throws Exception {
 
+            from("ftp://172.31.245.38/orders?username=" + ftpuser + "&password=" + ftppass + "&move=processed/${file:name}")
+                    .to("jms:incomingOrders");
+
+            from("jms:incomingOrders")
+                    .choice()
+                    .when(header("CamelFileName").endsWith(".xml"))
+                    .to("jms:xmlOrders")
+                    .when(header("CamelFileNAme").endsWith(".csv"))
+                    .to("jms:csvOrders")
+                    .otherwise()
+                    .to("jms:badOrders");
+
+            from("jms:xmlOrders")
+                    .bean(RecipientsBean.class)
+                    .log("Received XML order: ${header.CamelFileName}");
+
+            // adding pipeline to prod and accounting
+            from("jms:production")
+                    .log("Production received ${header.CamelFileName}");
+
+            from("jms:accounting")
+                    .log("Accounting received ${header.CamelFileName}");
+
+
+            from("jms:csvOrders")
+                    .log("Received CSV order: ${header.CamelFileName}")
+                    .to("file:data/outbox/csv");
+
+
+            from("jms:badOrders")
+                    .log("Received a bad order: ${header.CamelFileName}")
+                    .to("file:data/outbox/badOrders");
+
+            from("jms:continuedProcessing")
+                    .log("order will continue processing: ${header.CamelFileName}");
+        }
+    }
+
+    static class RecipientsBean {
+
+        @RecipientList
+        public String[] recipients(@XPath("/order/@customer") String customer) {
+            if (customer.equals("honda")) {
+                return new String[] { "jms:accounting", "jms:production" };
+            } else {
+                return new String[] { "jms:accounting" };
+            }
+        }
+    }
+
+
+    // using multicasting
+    static class CBRTestRouteWithFilterAndMulticast extends RouteBuilder {
+
+        @Override
+        public void configure() throws Exception {
+
+            from("ftp://172.31.245.38/orders?username=" + ftpuser + "&password=" + ftppass + "&move=processed/${file:name}")
+                    .to("jms:incomingOrders");
+
+            from("jms:incomingOrders")
+                    .choice()
+                    .when(header("CamelFileName").endsWith(".xml"))
+                    .to("jms:xmlOrders")
+                    .when(header("CamelFileNAme").endsWith(".csv"))
+                    .to("jms:csvOrders")
+                    .otherwise()
+                    .to("jms:badOrders");
+
+            from("jms:xmlOrders")
+//                    .filter(xpath("/order[not(@test)]"))
+                    .log("Received XML order: ${header.CamelFileName}")
+                    // multicasting
+                    .multicast()
+                    // for parallel processing, by default it is sequential
+//                    .parallelProcessing()
+                    .stopOnException()
+                    .to("jms:production", "jms:accounting", "file:data/outbox/xml");
+
+            // adding pipeline to prod and accounting
+            from("jms:production")
+                    // adding this to test if stopOnException works
+//                    .throwException(Exception.class, "I failed!!!")
+                    .log("Production received ${header.CamelFileName}");
+
+            from("jms:accounting")
+                    .log("Accounting received ${header.CamelFileName}");
+
+
+            from("jms:csvOrders")
+                    .log("Received CSV order: ${header.CamelFileName}")
+                    .to("file:data/outbox/csv");
+
+
+            from("jms:badOrders")
+                    .log("Received a bad order: ${header.CamelFileName}")
+                    .to("file:data/outbox/badOrders");
+
+            from("jms:continuedProcessing")
+                    .log("order will continue processing: ${header.CamelFileName}");
+        }
+    }
+
+
+    static class CBRTestRouteWithFilter extends RouteBuilder {
+
+        @Override
+        public void configure() throws Exception {
 
             from("ftp://172.31.245.38/orders?username=" + ftpuser + "&password=" + ftppass + "&move=processed/${file:name}")
                     .to("jms:incomingOrders");
